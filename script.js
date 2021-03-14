b52ce51927c8dc5df3bff7176003b802b03f8445
Gabriele Salvo <72619479+gabsalvo@users.noreply.github.com>
2021-03-14 13:32:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 let capital = 1000;
 const target = 1000000;
 
-let average = 0.1;
+let average = 1.1;
 let investment = capital * average + capital;
 const arrResults = [capital];
 
-for (let i = investment; i <= 1000000; i = i + i * 0.1) {
+for (let i = investment; i <= 1000000; i = i * 1.1) {
   arrResults.push(Math.trunc(i));
+  //MARIANNA HELPED ME WITH THE COMPUTATION, THANKS <3
 }
 
 console.log(arrResults.length);
